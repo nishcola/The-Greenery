fb23b8372fd795fc9d29320d193b791584a8c89e
nishcola <144401361+nishcola@users.noreply.github.com>
2025-04-08 16:28:04 -0400

    animations and improvements
    TODO: blog pages

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 800, // values from 0 to 3000, with step 50ms
+    once: true, // whether animation should happen only once - while scrolling down
+  });
   // --- Mock Order Page Logic ---
   const orderForm = document.getElementById("mockOrderForm"); // Get the form element
 
